a3d700d7f5942e8054161fa9ef74353c1dd2fe53
Jonathan Perry <jonathan.perry@rma-consulting.com>
2016-08-16 16:42:21 +0100

    UX

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const commander = require('commander')
 http://stackoverflow.com/questions/9153571/is-there-a-way-to-get-version-from-package-json-in-nodejs-code
 */
 const pkg = JSON.parse(require('fs').readFileSync('package.json', 'utf8'))
-const run = require('./lib').run
+const lib = require('./lib')
 
 commander
   .version(pkg.version)
   .option('-s, --src_path <src_path>', 'The location from which to read font files')
   .option('-c, --css_path <css_path>', 'The location at which to write CSS files')
+  .option('--silent', 'Silent running')
   .parse(process.argv)
 
-run(commander.src_path, commander.css_path)
+lib.runCmd(commander.silent, commander.src_path, commander.css_path)
   .then(() => process.exit())
 
-module.exports = run
+module.exports = lib.run
